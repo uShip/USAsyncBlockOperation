dd10aaf652e56e0fa58a65c05da10195b1a5235d
Jason Pepas <jpepas@uship.com>
2015-12-01 15:39:09 -0600

    removing comments in favor of README.md.  adding license text and reference to github page.

--- USAsyncBlockOperation.swift
+++ USAsyncBlockOperation.swift
@@ -1,152 +1,14 @@
 //
 //  USAsyncBlockOperation.swift
-//  uShip
+//  See https://github.com/uShip/USAsyncBlockOperation
 //
 //  Created by Jason Pepas on 9/21/15.
 //  Copyright (c) 2015 uShip. All rights reserved.
 //
+//  Released under the terms of the MIT License.
+//  See https://opensource.org/licenses/MIT
 
 import Foundation
-
-/*
-
-USAsyncBlockOperation and USAsyncMainBlockOperation
----------------------------------------------------
-
-
-Problem: NSBlockOperation isn't compatible with asynchronous operations.
-
-As soon as the block returns, operation.finished becomes true, and the operation is dequeued and
-released.  This completely breaks things like using NSOperationQueue.maxConcurrentOperationCount
-
-
-Solution: The block must have the responsibility of setting operation.finished = true
-
-USAsyncBlockOperation and USAsyncMainBlockOperation implement this solution.
-
-
-Here is a trivial example:
-
-
-class Thing
-{
-    let queue = NSOperationQueue()
-    let service = SlowNetworkService()
-    
-    func doIt()
-    {
-        var op = USAsyncBlockOperation()
-        
-        op.asynchronousExecutionBlock = { [weak self, weak op] () -> Void in
-            
-            self?.service.getData({ (data: String) -> () in
-                
-                debugPrintln("Data recieved: \(data)")
-                op?.asynchronousPortionIsFinished = true
-                
-            })
-        }
-        
-        queue.addOperation(op)
-    }
-}
-
-
-The above illustrates correct usage, but doesn't give you anything you can't already get with NSBlockOperation.
-
-
-Here is a full working example.  Create a new "Single View" Xcode project and replace the stock ViewController
-implementation with the following:
-
-
-import UIKit
-
-class ViewController: UIViewController
-{
-    let thing = Thing2()
-    
-    override func viewDidLoad()
-    {
-        super.viewDidLoad()
-        thing.doItTenTimes()
-    }
-}
-
-class Thing2
-{
-    let queue = NSOperationQueue()
-    let service = SlowNetworkService()
-    
-    init()
-    {
-        queue.maxConcurrentOperationCount = 2
-    }
-    
-    func doIt()
-    {
-        var op = USAsyncBlockOperation()
-        
-        op.asynchronousExecutionBlock = { [weak self, weak op] () -> Void in
-            
-            self?.service.getData({ (data: String) -> () in
-                
-                debugPrintln("Data recieved: \(data)")
-                op?.asynchronousPortionIsFinished = true
-                
-            })
-        }
-        
-        queue.addOperation(op)
-    }
-    
-    func doItTenTimes()
-    {
-        for i in 1...10 {
-            doIt()
-        }
-    }
-}
-
-class SlowNetworkService
-{
-    func getData(completion:((data: String)->()))
-    {
-        dispatch_after(1.0, dispatch_get_main_queue()) { () -> () in
-            completion(data: "Hello, World!")
-        }
-    }
-}
-
-func dispatch_after(delay: Double, queue: dispatch_queue_t, closure: ()->())
-{
-    let dtime = dispatch_time(DISPATCH_TIME_NOW, Int64(delay * Double(NSEC_PER_SEC)))
-    dispatch_after(dtime, queue) { () -> Void in
-        closure()
-    }
-}
-
-
-The above demonstrates rate-limiting asynchronous operations by using NSOperationQueue.maxConcurrentOperationCount.
-
-Two requests will be issued (each on their own background thread), and the next two will not start until
-asynchronousPortionIsFinished is set to true for the first two requests.
-
-Note also the use of [weak self, weak op], which prevents a retain cycle between the operation and Thing2.
-
-
-USAsyncMainBlockOperation:
-
-Do not use USAsyncBlockOperation on NSOperationQueue.mainQueue.  This is a serial queue, and your long-running
-asynchronous operations will block up the mainQueue while running one at a time.
-
-Instead, define a regular NSOperationQueue, but fill it with USAsyncMainBlockOperations.  These will immedately
-dispatch onto the main thread, but will also abide by NSOperationQueue.maxConcurrentOperationCount.
-
-
-see also https://developer.apple.com/library/ios/documentation/Cocoa/Reference/NSOperation_class/
-see also http://nshipster.com/nsoperation/
-
-*/
 
 class USAsyncBlockOperation: NSOperation
 {
@@ -229,4 +91,3 @@ class USAsyncMainBlockOperation: USAsyncBlockOperation
         }
     }
 }
-
